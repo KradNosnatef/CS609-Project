c024e5a1391472939a7d999c131b998b0d9e88c6
KradNosnatef <fuqianshan@foxmail.com>
2023-10-26 21:48:13 +0800

    very first update

---
+++ PPOTest/ppo.py
@@ -0,0 +1,7 @@
+from network import FeedForwardNN
+
+class PPO:
+    def __init__(self,env):
+        self.env=env
+        self.obs_dim=env.observation_space.shape[0]
+        self.act_dim=env.action_space.shape[0]
